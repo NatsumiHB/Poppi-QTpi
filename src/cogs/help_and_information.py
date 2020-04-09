621258e3009581b69ab554cfcd93ab3a3b6366c2
Natsumi H <39241405+NatsumiHB@users.noreply.github.com>
2020-04-09 23:12:26 +0200

    Please work

--- src/cogs/help_and_information.py
+++ src/cogs/help_and_information.py
@@ -8,7 +8,23 @@ class HelpAndInformation(commands.Cog, name="Help and Information"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @command(help="Get someone's avatar", usage="[mention]")
+    @command(help="Display this")
+    async def help(self, ctx):
+        embed = discord.Embed(title=f"Help for {self.bot.user.display_name}", color=discord.Color.purple())
+        embed.set_thumbnail(url=self.bot.user.avatar_url)
+
+        for cog in self.bot.cogs:
+            commands = ""
+            for command in self.bot.get_cog(cog).get_commands():
+                commands += f"{command.name} -> {command.help}\n"
+
+            embed.add_field(name=cog, value=commands, inline=False)
+
+        embed.set_footer(text=f"{self.bot.user.display_name} | Ping: {round(self.bot.latency, 1)}ms")
+
+        await ctx.send(embed=embed)
+
+    @command(help="Get someone's avatar")
     async def avatar(self, ctx, user: discord.User):
         embed = discord.Embed(color=discord.Color.purple())
         embed.set_author(name=f"{user.display_name}'s avatar", url=str(user.avatar_url))
@@ -16,7 +32,7 @@ class HelpAndInformation(commands.Cog, name="Help and Information"):
 
         await ctx.send(embed=embed)
 
-    @command(help="Information about me", usage="")
+    @command(help="Information about me")
     async def info(self, ctx):
         embed = discord.Embed(color=discord.Color.purple(), description=f"I'm on {len(self.bot.guilds)} servers!")
         embed.set_author(name=f"Information about {self.bot.user.display_name}", icon_url=self.bot.user.avatar_url)
@@ -24,7 +40,7 @@ class HelpAndInformation(commands.Cog, name="Help and Information"):
 
         await ctx.send(embed=embed)
 
-    @command(help="Send a support DM to my creator", usage="[msg]")
+    @command(help="Send a support DM to my creator")
     async def support(self, ctx, *, msg):
         user = self.bot.get_user(self.bot.owner_id)
         await user.send(f"Support request from {ctx.author.display_name}#{ctx.author.discriminator}:\n\n```{msg}```")
---
+++ update.sh
@@ -0,0 +1,1 @@
+docker-compose down && docker pull natsuwumi/poppi && docker-compose up -d && docker logs -f poppiqtpi_poppi_1
